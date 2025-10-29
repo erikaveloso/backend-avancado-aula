95e4fc349d4248fe2fae0e9e52729b7575803622
Erika Veloso <erikaveloso@msn.com>
2025-10-29 09:30:56 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/controller/UsuarioController.java
@@ -0,0 +1,45 @@
+package med.voll.api.controller;
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosCadastroUsuario;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("cadastros")
+public class UsuarioController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastro (@RequestBody @Valid
+                                    DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder){
+
+        if (repository.findByLogin(dados.login()) !=null){
+            return ResponseEntity.badRequest().body("Login já cadastrado!");
+        }
+
+        var senhaCriptografada = passwordEncoder.encode(dados.senha());
+        var usuario = new Usuario(dados.login(), senhaCriptografada);
+
+        repository.save(usuario);
+        return null;
+
+    }
+
+}
+
---
+++ src/main/java/med/voll/api/domain/usuario/DadosCadastroUsuario.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosCadastroUsuario(
+        @NotBlank(message = "Login é obrigatório!") String login,
+        @NotBlank(message = "Senha é obrigatório!") String senha
+) {
+}
